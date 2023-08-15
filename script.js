15ce418c5f13b00e6561579e6b21212e233eea0f
canbarinn <canbarinn@gmail.com>
2023-08-16 01:33:49 +0300

    delete fav item and add video

--- script.js
+++ script.js
@@ -1,109 +1,143 @@
 "use strict";
 
-let API_KEY = "s8WQQgzdfVNLP3dhSDb2Y0UyGp7Ad67DNOYlsak3";
+let API_KEY = "5tQRcYjsycg4XlKGHpFSuQ3aUlmRikilmqLQz0bK";
 
-let picArray = []
+let picArray = [];
 
 async function getPics() {
   let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=10`;
   let response = await fetch(url);
   let data = await response.json();
-  console.log(data)
+  console.log(data);
   return data;
 }
 
 async function addSpaceItems() {
-  let i =0;
-  let array = await getPics()
-  let ul = document.getElementById("space-list")
-  await array.map((item)=> {
+  let i = 0;
+  let array = await getPics();
+  array.map((element) => {
+    if (element.media_type !== "image") {
+    }
+  });
+  let ul = document.getElementById("space-list");
+  await array.map((item) => {
     picArray.push(item);
-    let {title:title, url:url, explanation:explanation, copyright:copyright, date:date} = item
+    let { title: title, url: url, explanation: explanation, copyright: copyright, date: date } = item;
     // console.log(title,url,explanation)
-    let li = document.createElement("li")
-    li.id = i
-    let html = `
-    <img src=${url} alt="Space image" class="image">
-    <p id="reference"><em>${copyright ? `Reference: ${copyright }, ` : ""} Date: ${date}</em><p/>
-    <h1 class="header">${title}</h1>
-    <text class="text">${explanation}</text>
-    <p class="foot-info"></p>
-    <button class="fav-button" onclick="getItem(${i})">ADD TO FAVORITES</button>
-    `
-    li.innerHTML = html
-    ul.appendChild(li)
-    i++
-  })
+    let li = document.createElement("li");
+    li.id = i;
+    let html = "";
+    if (item.media_type === "video") {
+      html = `
+      <iframe class="iframe" src=${url} allowfullscreen> </iframe>
+      <p id="reference"><em>${copyright ? `Reference: ${copyright}, ` : ""} Date: ${date}</em><p/>
+      <h1 class="header">${title}</h1>
+      <text class="text">${explanation}</text>
+      <p class="foot-info"></p>
+      <button class="fav-button foot-btn" onclick="getItem(${i})">ADD TO FAVORITES</button>
+      `;
+    } else {
+      html = `
+      <img src=${url} alt="Space image" class="image">
+      <p id="reference"><em>${copyright ? `Reference: ${copyright}, ` : ""} Date: ${date}</em><p/>
+      <h1 class="header">${title}</h1>
+      <text class="text">${explanation}</text>
+      <p class="foot-info"></p>
+      <button class="fav-button foot-btn" onclick="getItem(${i})">ADD TO FAVORITES</button>
+      `;
+    }
+
+    li.innerHTML = html;
+    ul.appendChild(li);
+    i++;
+  });
 }
 
-window.addEventListener("load", function() {
-  addSpaceItems()
-  showFavoriteItems()
-
+window.addEventListener("load", function () {
+  addSpaceItems();
+  showFavoriteItems();
 });
 
-
-let favsArray = []
+let favsArray = [];
 
 const getItem = function (id) {
-let message = document.querySelector(".added-message-container");
-let cannotMessage = document.querySelector(".cannot-message-container");
+  let message = document.querySelector(".added-message-container");
+  let cannotMessage = document.querySelector(".cannot-message-container");
 
-let getjson = localStorage.getItem("favSpaceItems")
-let olddata = JSON.parse(getjson)
+  let getjson = localStorage.getItem("favSpaceItems");
+  let olddata = JSON.parse(getjson);
 
-if(!favsArray.includes(picArray[id])){
-  favsArray = [...olddata]
-  favsArray.push(picArray[id])
-  saveToLocalStorage(favsArray)
-  message.hidden = false;
-  setTimeout(()=> {
-    message.hidden  = true;
-
-  },2000)
-} else {
-  cannotMessage.hidden = false
-  setTimeout(() => {
-    cannotMessage.hidden = true
-  },2000)
-}
-
-}
+  if (!favsArray.includes(picArray[id])) {
+    if (olddata) {
+      favsArray = [...olddata];
+    }
+    favsArray.push(picArray[id]);
+    saveToLocalStorage(favsArray);
+    message.hidden = false;
+    setTimeout(() => {
+      message.hidden = true;
+    }, 2000);
+  } else {
+    cannotMessage.hidden = false;
+    setTimeout(() => {
+      cannotMessage.hidden = true;
+    }, 2000);
+  }
+};
 // WORKING
 function saveToLocalStorage(array) {
+  let json = JSON.stringify(array);
+  localStorage.setItem("favSpaceItems", json);
 
-  let json = JSON.stringify(array)
-  localStorage.setItem("favSpaceItems", json)
-
-  let getjson = localStorage.getItem("favSpaceItems")
-  let data = JSON.parse(getjson)
-  console.log("localstorage data", data)
-
+  let getjson = localStorage.getItem("favSpaceItems");
+  let data = JSON.parse(getjson);
+  console.log("localstorage data", data);
 }
 
-
 function getItemsFromLocalStorage() {
-  let jsonArray = localStorage.getItem("favSpaceItems")
-  let array = JSON.parse(jsonArray)
-  return array
+  let jsonArray = localStorage.getItem("favSpaceItems");
+  let array = JSON.parse(jsonArray);
+  return array;
 }
 
 function showFavoriteItems() {
-  let items = getItemsFromLocalStorage()
-  let ul = document.getElementById("favorites-list")
-  items.map(item => {
-    let {title:title, url:url, explanation:explanation} = item
+  let items = getItemsFromLocalStorage();
+  let ul = document.getElementById("favorites-list");
+
+  items.map((item) => {
+    let { title: title, url: url, explanation: explanation, copyright: copyright, date: date } = item;
     // console.log(title,url,explanation)
-    let li = document.createElement("li")
-    let html = `<h1 class="header">${title}</h1>
+    let li = document.createElement("li");
+    let html = "";
+    if (item.media_type === "video") {
+      html = `
+      <iframe class="iframe" src=${url} allowfullscreen> </iframe>
+      <p id="reference"><em>${copyright ? `Reference: ${copyright}, ` : ""} Date: ${date}</em><p/>
+      <h1 class="header">${title}</h1>
+      <text class="text">${explanation}</text>
+      <p class="foot-info"></p>
+      <button type="submit" class="delete-fav foot-btn" onclick="deleteFromFavorites(${items.indexOf(item)})">REMOVE</button>
+      `;
+    } else {
+      html = `
     <img src=${url} alt="Space image" class="image">
-    <h2 class="text-header">XYZ</h2>
+    <p id="reference"><em>${copyright ? `Reference: ${copyright}, ` : ""} Date: ${date}</em><p/>
+    <h1 class="header">${title}</h1>
     <text class="text">${explanation}</text>
     <p class="foot-info"></p>
-  })`
-  li.innerHTML = html
-  ul.appendChild(li)
-
-})
+    <button type="submit" class="delete-fav foot-btn" onclick="deleteFromFavorites(${items.indexOf(item)})">REMOVE</button>
+    `;
+    }
+    li.innerHTML = html;
+    ul.appendChild(li);
+  });
 }
 
+function deleteFromFavorites(index) {
+  let data = getItemsFromLocalStorage();
+  console.log(data);
+  data.splice(index, 1);
+  let json = JSON.stringify(data);
+  localStorage.setItem("favSpaceItems", json);
+  window.location.reload();
+}
